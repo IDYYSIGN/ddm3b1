637a279ec3df47272ed46e8a52ef58ab09378b50
IDYYSIGN <leidilene.batista@etec.sp.gov.br>
2024-03-07 10:52:51 -0300

    Rota

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Title from './src/components';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Leidi 29/02/24</Text>
+      <Text>Clone do projeto aula 04-03</Text>
+      <title>Titulo 3b1 lado b</title>
       <StatusBar style="auto" />
     </View>
   );
